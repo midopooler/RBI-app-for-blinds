50137a80f44aaa12f09b0aa84c9e80081d03de6a
Pulkit Midha <midhapulkit28@gmail.com>
2019-11-27 23:41:23 +0530

    Add files via upload

---
+++ AudioActive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioActive : MonoBehaviour
+{
+    MeshRenderer MR;
+    AudioSource AS;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MR = transform.GetComponent<MeshRenderer>();
+        AS = transform.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MR.enabled == true)
+        {
+            AS.enabled = true;
+        }
+        else
+        {
+            AS.enabled = false;
+        }
+    }
+}
